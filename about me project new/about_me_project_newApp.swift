0b0b37b35e192864dc3baeed4ed8a0f63f524be4
Christa Kulanda <kulanda@gmail.com>
2023-04-22 22:24:43 -0500

    Initial Commit

---
+++ about me project new/about_me_project_newApp.swift
@@ -0,0 +1,17 @@
+//
+//  about_me_project_newApp.swift
+//  about me project new
+//
+//  Created by scholar on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct about_me_project_newApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
